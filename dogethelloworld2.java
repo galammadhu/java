b8ff74e00ad640e1e988ddef01dadf9c9ac186a9
galammadhu <161491582+galammadhu@users.noreply.github.com>
2024-03-31 22:01:03 +0530

    Create dogethelloworld2.java

---
+++ dogethelloworld2.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld2 extends HttpServlet {
+
+protected void doGet(HttpServletRequest request, 
+HttpServletResponse response) throws ServletException, IOException {
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+ out.println("<h1>Hello World using HttpServlet class with doGet.</h1>");
+ out.close();
+
+ }
+}
