322689a3c312114283476ca93bd7bfb7e5cfbfda
galammadhu <161491582+galammadhu@users.noreply.github.com>
2024-03-31 21:58:25 +0530

    Create login.java

---
+++ login.java
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>IARE Registration</title>
+	<link rel="stylesheet"
+		href="style.css">
+</head>
+
+<body>
+	<div class="main">
+		<h1>IARE Student Registration</h1>
+		<h3>Enter your login credentials</h3>
+		<form action="LoginServlet1">
+			<label for="first">
+				Username:
+			</label>
+			<input type="text"
+				id="first"
+				name="first"
+				placeholder="Enter your Username" required>
+
+			<label for="password">
+				Password:
+			</label>
+			<input type="password"
+				id="password"
+				name="password"
+				placeholder="Enter your Password" required>
+
+			<div class="wrap">
+				<button type="submit"
+						onclick="solve()">
+					Submit
+				</button>
+			</div>
+		</form>
+		<p>Not registered? 
+			<a href="#"
+			style="text-decoration: none;">
+				Create an account
+			</a>
+		</p>
+	</div>
+</body>
+
+</html>
